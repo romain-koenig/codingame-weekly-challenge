8fa654b7540675a94d4cf1ce1e79932ed51e3ff9
Romain KOENIG <romain.koenig@gmail.com>
2020-06-13 15:45:08 +0200

    All tests OK

--- SOLO_EASY/1D_Spreadsheet.test.js
+++ SOLO_EASY/1D_Spreadsheet.test.js
@@ -37,62 +37,97 @@ let spread = []
 for (let i = 0; i < N; i++) {
     var inputs = readline().split(' ');
     const operation = inputs[0];
-    const arg1 = sanytizeInput(1);
-    const arg2 = sanytizeInput(2);
+
+    let arg1 = null, arg2 = null, arg1_index = null, arg2_index = null;
+
+    arg1 = inputs[1] === "_" ? null : (inputs[1].startsWith('$') ? null : parseInt(inputs[1]));
+    arg2 = inputs[2] === "_" ? null : (inputs[2].startsWith('$') ? null : parseInt(inputs[2]));
+
+    arg1_index = inputs[1].startsWith('$') ? parseInt(inputs[1].substring(1)) : null;
+    arg2_index = inputs[2].startsWith('$') ? parseInt(inputs[2].substring(1)) : null;
+
     spread.push({
         operation: operation,
         arg1: arg1,
-        arg2: arg2
+        arg1_index: arg1_index,
+        arg2: arg2,
+        arg2_index: arg2_index,
+        evaluated: false
     })
 }
 
-test('Check if line initialized as it should', () => {
+
+while (spread.reduce((acc, curr) => acc && curr.evaluated, true) == false) {
+
+    for (let i = 0; i < spread.length; i++) {
+        //We start by evaluating the easy ones
+        if ((spread[i].operation === OPERATIONS.VALUE && spread[i].arg1 !== null)
+            || (spread[i].arg1 !== null && spread[i].arg2 !== null)) {
+
+            switch (spread[i].operation) {
+                case OPERATIONS.VALUE:
+                    spread[i].outputValue = spread[i].arg1;
+                    break;
+                case OPERATIONS.ADD:
+                    spread[i].outputValue = spread[i].arg1 + spread[i].arg2;
+                    break;
+                case OPERATIONS.MULTIPLY:
+                    spread[i].outputValue = spread[i].arg1 * spread[i].arg2;
+                    break;
+                case OPERATIONS.SUBSTRACT:
+                    spread[i].outputValue = spread[i].arg1 - spread[i].arg2;
+                    break;
+
+                default:
+                    break;
+            }
+            spread[i].evaluated = true;
+        }
+        //else, we try to retrieve data in an evaluated cell
+        else {
+            if (spread[i].arg1_index != null && spread[spread[i].arg1_index].evaluated == true) {
+                spread[i].arg1 = spread[spread[i].arg1_index].outputValue;
+            }
+            if (spread[i].arg2_index != null && spread[spread[i].arg2_index].evaluated == true) {
+                spread[i].arg2 = spread[spread[i].arg2_index].outputValue;
+            }
+        }
+    }
+    // we loop until everything is evaluated
+}
+
+
+// printing the results
+for (let i = 0; i < N; i++) {
+
+    console.log(spread[i].outputValue == -0 ? 0 : spread[i].outputValue);
+}
+
+
+
+test('Check if return value is OK', () => {
     expect(spread).toStrictEqual([{
         operation: OPERATIONS.VALUE,
         arg1: 3,
-        arg2: null
+        arg1_index: null,
+        arg2: null,
+        arg2_index: null,
+        outputValue: 3,
+        evaluated: true,
     },
     {
         operation: OPERATIONS.ADD,
-        arg1: "$0",
-        arg2: 4
+        arg1: 3,
+        arg1_index: 0,
+        arg2: 4,
+        arg2_index: null,
+        outputValue: 7,
+        evaluated: true,
     }])
 })
-
-for (let i = 0; i < N; i++) {
-    switch (spread[i].operation) {
-        case OPERATIONS.VALUE:
-            spread[i].outputValue = spread[i].arg1;
-            break;
-        case OPERATIONS.ADD:
-            spread[i].outputValue = spread[i].arg1 + spread[i].arg2;
-            break;
-        case OPERATIONS.MULTIPLY:
-            spread[i].outputValue = spread[i].arg1 * spread[i].arg2;
-            break;
-        case OPERATIONS.SUBSTRACT:
-            spread[i].outputValue = spread[i].arg1 - spread[i].arg2;
-            break;
-
-        default:
-            break;
-    }
-}
-
-for (let i = 0; i < N; i++) {
-
-    console.log(spread[i].outputValue);
-}
-
-
-
 
 
 test('DOING NOTHING - JEST needs at least ONE test', () => {
     expect(true).toBe(true);
 }
 )
-function sanytizeInput(index) {
-    return inputs[index] === "_" ? null : (inputs[index].startsWith('$') ? inputs[index] : parseInt(inputs[index]));
-}
-
