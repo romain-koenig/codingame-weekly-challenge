8f7cc7ccfb7aac745c1fb12e96ce94765ae9d2b9
Romain KOENIG <romain.koenig@gmail.com>
2020-06-15 13:01:29 +0200

    first bricks

--- SOLO_EASY/GhostLegs.test.js
+++ SOLO_EASY/GhostLegs.test.js
@@ -1,20 +1,35 @@
-/**
+/*
  * Simulate input from real situation
  */
 
 
-let line = 0;
+let line = -1;
 const readline = () => {
     line++;
     switch (line) {
+        case 0:
+            return "7 7";
+            break;
         case 1:
-            return "2";
+            return "A  B  C";
             break;
         case 2:
-            return "VALUE 3 _";
+            return "|  |  |";
             break;
         case 3:
-            return "ADD $0 4";
+            return "|--|  |";
+            break;
+        case 4:
+            return "|  |--|";
+            break;
+        case 5:
+            return "|  |--|";
+            break;
+        case 6:
+            return "|  |  |";
+            break;
+        case 7:
+            return "1  2  3";
             break;
         default:
             break;
@@ -28,21 +43,58 @@ const readline = () => {
  * the standard input according to the problem statement.
  **/
 
-var inputs = readline().split(' ');
+const inputs = readline().split(' ');
 const W = parseInt(inputs[0]);
 const H = parseInt(inputs[1]);
+
+
+//reading top row
+const headValues = readline().split('  ');
+
+//reading the game grid
+const gameGrid = [];
+
 for (let i = 0; i < H; i++) {
-    const line = readline();
+    const line = readline().replace("  ", " ").replace("--", "-");
     console.error(line);
+    gameGrid.push(line);
 }
+
+//Bottom row
+const downValues = readline().split('  ');
+
 
 // Write an answer using console.log()
 // To debug: console.error('Debug messages...');
+let answer = [];
 
-console.log('answer');
+answer.map(e => console.log(e));
 
 
 test('DOING NOTHING - JEST needs at least ONE test', () => {
     expect(true).toBe(true);
+}
+)
+
+
+test('Testing header values', () => {
+    const expectedHederValues = ["A", "B", "C"]
+    expect(headValues).toStrictEqual(expectedHederValues);
+}
+)
+
+test('Testing groung values', () => {
+    const expectedDownValues = [["| | |"],
+    ["|-| |"],
+    ["| |-|"],
+    ["| |-|"],
+    ["| | |"]];
+    expect(downValues).toStrictEqual(expectedDownValues);
+}
+)
+
+test('Checking the final answer of the game', () => {
+    const expectedAnswer = ["A2", "B1", "C3"]
+    expect(answer).toStrictEqual(expectedAnswer);
 }
 )
