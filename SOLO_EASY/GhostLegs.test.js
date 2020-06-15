4879aa39653ac99be3687529d26bf590439993d4
Romain KOENIG <romain.koenig@gmail.com>
2020-06-15 15:32:58 +0200

    calcul chemins OK

--- SOLO_EASY/GhostLegs.test.js
+++ SOLO_EASY/GhostLegs.test.js
@@ -37,6 +37,14 @@ const readline = () => {
 }
 
 
+// ██████╗ ███████╗ ██████╗ ██╗███╗   ██╗
+// ██╔══██╗██╔════╝██╔════╝ ██║████╗  ██║
+// ██████╔╝█████╗  ██║  ███╗██║██╔██╗ ██║
+// ██╔══██╗██╔══╝  ██║   ██║██║██║╚██╗██║
+// ██████╔╝███████╗╚██████╔╝██║██║ ╚████║
+// ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝
+                                      
+
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -63,13 +71,44 @@ for (let i = 0; i < H - 2; i++) {
 const downValues = readline().split('  ');
 
 
+// translate read values into directional values
+const size = gameGrid.length;
+const width = headValues.length
+let chemins = [];
+for (let i = 0; i < size; i++) {
+    let chemin = new Array(0, 0, 0);
+
+    chemins.push(chemin);
+
+    for (let j = 0; j < width - 1; j++) {
+        console.error(`i - ${i}, j - ${j}`);
+        let currentChar = gameGrid[i].substring(2 * j + 1, 2 * j + 2);
+        console.error(currentChar);
+        if (currentChar === "-") {
+            console.error(`CurrentChar is -`)
+            console.error(chemins);
+            chemins[i][j] = 1;
+            chemins[i][j + 1] = -1;
+        }
+    }
+}
+
 // Write an answer using console.log()
 // To debug: console.error('Debug messages...');
 let answer = [];
 
 gameGrid.map(e => console.error(e));
+chemins.map(e => console.error(e));
 answer.map(e => console.log(e));
 
+
+// ███████╗███╗   ██╗██████╗ 
+// ██╔════╝████╗  ██║██╔══██╗
+// █████╗  ██╔██╗ ██║██║  ██║
+// ██╔══╝  ██║╚██╗██║██║  ██║
+// ███████╗██║ ╚████║██████╔╝
+// ╚══════╝╚═╝  ╚═══╝╚═════╝ 
+                          
 
 test('DOING NOTHING - JEST needs at least ONE test', () => {
     expect(true).toBe(true);
@@ -97,6 +136,16 @@ test('Testing grid values', () => {
     expect(gameGrid).toStrictEqual(expectedGridValues);
 }
 )
+
+test('Check chemins', () => {
+    const expectedValues = [
+        [0, 0, 0],
+        [1, -1, 0],
+        [0, 1, -1],
+        [0, 1, -1],
+        [0, 0, 0]];
+    expect(chemins).toStrictEqual(expectedValues);
+})
 
 test('Checking the final answer of the game', () => {
     const expectedAnswer = ["A2", "B1", "C3"]
