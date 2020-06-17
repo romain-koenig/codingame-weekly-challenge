81be7ebe0a17c9e3830b689fddd9769b5d63ae9b
Romain KOENIG <romain.koenig@gmail.com>
2020-06-17 15:06:18 +0200

    starting point

---
+++ SOLO_MEDIUM/ANEOSponsoredPuzzle.test.js
@@ -0,0 +1,53 @@
+let line = 0;
+
+const readline = () => {
+    line++;
+
+    switch (line) {
+        case 1: return 50; break;
+        case 2: return 1; break;
+        case 3: return 200 15; break;
+
+    }
+}
+
+// ██████╗ ███████╗ ██████╗ ██╗███╗   ██╗
+// ██╔══██╗██╔════╝██╔════╝ ██║████╗  ██║
+// ██████╔╝█████╗  ██║  ███╗██║██╔██╗ ██║
+// ██╔══██╗██╔══╝  ██║   ██║██║██║╚██╗██║
+// ██████╔╝███████╗╚██████╔╝██║██║ ╚████║
+// ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝
+   
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const speed = parseInt(readline());
+const lightCount = parseInt(readline());
+for (let i = 0; i < lightCount; i++) {
+    var inputs = readline().split(' ');
+    const distance = parseInt(inputs[0]);
+    const duration = parseInt(inputs[1]);
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
+
+
+// ███████╗███╗   ██╗██████╗ 
+// ██╔════╝████╗  ██║██╔══██╗
+// █████╗  ██╔██╗ ██║██║  ██║
+// ██╔══╝  ██║╚██╗██║██║  ██║
+// ███████╗██║ ╚████║██████╔╝
+// ╚══════╝╚═╝  ╚═══╝╚═════╝ 
+
+
+
+test('DOING NOTHING - JEST needs at least ONE test', () => {
+    expect(true).toBe(true);
+}
+)
